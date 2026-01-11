16605f9c139d754655fc5cd0af43f8e5bb9e52ef
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-01-11 14:39:27 -0800

    update tests

--- tests/testthat/test-plot_trend.R
+++ tests/testthat/test-plot_trend.R
@@ -1,20 +1,20 @@
 test_that("plot trend results correct", {
   indat1 <- readRDS(test_path("testdata", "input1_test.rds"))
   ldf <-  readRDS(test_path("testdata", "fit_hgam_model.rds"))
-  tr = get_trend(ldf)
+  tr = get_trend(ldf, start_yr = 1968, end_yr = 1977, method = "gmean", annual_variation = FALSE)
   pred_sm =  proj_trend(ldf, tr, start_yr = 1978, proj_yr = 2020)
-  pl <- plot_trend(indat1, ldf, pred_sm, start_yr = 1968, end_yr = 1977, ref_yr = 1977 )
-  expect_silent(plot_trend(indat1, ldf, pred_sm, start_yr = 1968, end_yr = 1977, ref_yr = 1977))
+  pl <- plot_trend(indat1, ldf, pred_sm, start_yr = 1968, end_yr = 1977 )
+  expect_silent(plot_trend(indat1, ldf, pred_sm, start_yr = 1968, end_yr = 1977))
 })
 
 test_that("plot trendfails appropriately", {
   indat1 <- readRDS(test_path("testdata", "input1_test.rds"))
   ldf <-  readRDS(test_path("testdata", "fit_hgam_model.rds"))
-  tr = get_trend(ldf)
+  tr = get_trend(ldf,start_yr = 1968, end_yr = 1977, method = "gmean", annual_variation = FALSE)
   pred_sm =  proj_trend(ldf, tr, start_yr = 1978, proj_yr = 2020)
-  expect_message(plot_trend(indat1, ldf, pred_sm, start_yr = 1900, end_yr = 1977, ref_yr = 1977))
-  expect_message(plot_trend(indat1, ldf, pred_sm, start_yr = 1978, end_yr = 2020, ref_yr = 1977))
-  expect_error(plot_trend("a",ldf, pred_sm, start_yr = 1900, end_yr = 1977, ref_yr = 1977))
+  expect_message(plot_trend(indat1, ldf, pred_sm, start_yr = 1900, end_yr = 1977))
+  expect_message(plot_trend(indat1, ldf, pred_sm, start_yr = 1978, end_yr = 2020))
+  expect_error(plot_trend("a",ldf, pred_sm, start_yr = 1900, end_yr = 1977))
 })
 
 #testthat::test_file("tests/testthat/test-plot_trend.R")
--- tests/testthat/test-trend_change.R
+++ tests/testthat/test-trend_change.R
@@ -1,18 +1,18 @@
-test_that("get_trend fails appropriately", {
-  preds_sm = readRDS(test_path("testdata", "pred_tr_test.rds"))
-  cp = trend_change(preds_sm, ref_year = 1977, targ_year = 2011)
-  expect_silent(trend_change(preds_sm, ref_year = 1977, targ_year = 2011))
-  expect_equal(ncol(cp), 5)
-  #expect_equal(cp$prob_increase_5_percent, 100)
-})
-
-test_that("get_trend warnings appropriate", {
-  preds_sm = readRDS(test_path("testdata", "pred_tr_test.rds"))
-  expect_error(trend_change(preds_sm, ref_year = 1900, targ_year = 2011))
-  expect_error(trend_change(preds_sm, ref_year = 1977, targ_year = 1976))
-  expect_error(trend_change(preds_sm, ref_year = 1977, targ_year = "a"))
-  #expect_equal(ncol(cp), 3)
-  #expect_equal(cp$prob_increase_5_percent, 100)
-})
+# test_that("get_trend fails appropriately", {
+#   preds_sm = readRDS(test_path("testdata", "pred_tr_test.rds"))
+#   cp = trend_change(preds_sm, ref_year = 1977, targ_year = 2011)
+#   expect_silent(trend_change(preds_sm, ref_year = 1977, targ_year = 2011))
+#   expect_equal(ncol(cp), 5)
+#   #expect_equal(cp$prob_increase_5_percent, 100)
+# })
+#
+# test_that("get_trend warnings appropriate", {
+#   preds_sm = readRDS(test_path("testdata", "pred_tr_test.rds"))
+#   expect_error(trend_change(preds_sm, ref_year = 1900, targ_year = 2011))
+#   expect_error(trend_change(preds_sm, ref_year = 1977, targ_year = 1976))
+#   expect_error(trend_change(preds_sm, ref_year = 1977, targ_year = "a"))
+#   #expect_equal(ncol(cp), 3)
+#   #expect_equal(cp$prob_increase_5_percent, 100)
+# })
 
 #testthat::test_file("tests/testthat/test-trend_check.R")
